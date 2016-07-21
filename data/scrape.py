059e90ef1b363c589559bb29c25b7b08bb6ce2d0
Andrew Kamau <andkamau@gmail.com>
2016-07-21 16:28:39 +0300

    scraper enhancements

--- data/scrape.py
+++ data/scrape.py
@@ -7,13 +7,14 @@ API_KEY = os.getenv("IMPORTIO_API_KEY")
 #API = "https://api.import.io/store/connector/_magic?url={url}&format=JSON&js=false&_apikey={apikey}&_apikey={apikey}"
 API = "https://api.import.io/store/connector/_magic?url={url}&format=JSON&js=false&_apikey={apikey}"
 SOURCE = "http://online.lsk.or.ke/index.php/index.php?option=com_content&id=4&catid=8&qw=active&finder=Active&view=article&base=%s"
-PAGES = 204 # Get this from the site
-TIMEOUT = 23 # Request timeout in seconds
+#PAGES = 204 # Get this from the site
+PAGES = 2 # Get this from the site
+TIMEOUT = 35 # Request timeout in seconds
 redis_host = os.getenv('REDIS_HOST', 'localhost:6379')
 REDIS = dict(
         host=redis_host.split(':')[0],
         port=redis_host.split(':')[1],
-        db='3',
+        db='4',
         password=os.getenv('REDIS_PASSWORD', None),
         socket_timeout=2,
         socket_connect_timeout=2,
@@ -39,7 +40,21 @@ class LSKScraper(object):
             resp = response.json()
             results = resp['tables'][0]['results']
             for result in results:
-                print "{content_2} - {content_1}".format(**result)
+                result['link_1'] = result.get('link_1', "-")
+                try:
+                    result['name'] = result['content_1']
+                    result['number'] = result['content_2']
+                    result.pop("link_2/_source")
+                    result.pop("link_1/_source")
+                    result.pop("link_1/_text")
+                    result.pop("link_2/_text")
+                    result.pop("link_2")
+                    result.pop("content_1")
+                    result.pop("content_2")
+
+                    print result
+                except:
+                    print "ERROR: Skipped %s" % result
             print "Extracted %s names from page %s" % (len(results), page)
             return results
         except Exception, err:
@@ -51,7 +66,11 @@ def main():
     """
     Execute scraper
     """
+    lsk = LSKScraper()
     for page in range(1, PAGES+1):
-        url = SOURCE % str(page)
-        response = requests.get(url, timeout=TIMEOUT)
+        print "scraping page %s" % str(page)
+        results = lsk.scrape_page(str(page))
 
+
+if __name__ == "__main__":
+    main()
